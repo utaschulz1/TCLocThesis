bf632b22e0c9c118369bd5087dfb27a4f65a6c8a
[utaschulz1] <[uta.schulz1@gmail.com]>
2025-05-02 17:16:28 +0100

    production mode

--- lantern.sh
+++ lantern.sh
@@ -78,7 +78,6 @@ pdf() {
    $pandoc_command _temp/chapters.md \
         --defaults settings/pdf.yml \
         --output  $output_directory/$output_filename.pdf \
-        --verbose
     echo "📖 The PDF edition is now available in the $output_directory folder"
 }
 docx() {
